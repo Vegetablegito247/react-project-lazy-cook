1299459e6d14dc92457b7b0796eebb8d7955ebaf
Vegetablegito247 <Yoshimitsu247Kakarot@gmail.com>
2023-06-23 02:28:31 +0100

    sidebar

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,18 +1,25 @@
-import React from 'react';
+import React, { useState } from 'react';
 import { FaUtensils } from 'react-icons/fa';
 import { GiHamburgerMenu } from 'react-icons/gi';
 import { AiFillCloseSquare } from 'react-icons/ai';
+import { BsInstagram, BsGithub, BsFacebook, BsTwitter, BsTelegram } from 'react-icons/bs';
 import './styles/navbar.css';
 
 function Navbar() {
+  const [openSideBar, setSidebar] = useState(false);
+
+  const openModal = () => {
+    setSidebar(!openSideBar);
+  }
+
   return (
     <nav className="navbar">
       <div className="logo">
         <h3>Lazy Cook</h3>
         <FaUtensils className='uten' />
       </div>
-      <ul>
-        <div className="close">
+      <ul className={openSideBar ? 'nav-menu active' : 'nav-menu'}>
+        <div className="close" onClick={openModal}>
           <AiFillCloseSquare className='x-mark' />
         </div>
         <li>Home</li>
@@ -20,10 +27,17 @@ function Navbar() {
         <li>Our Chefs</li>
         <li>Request</li>
         <li>Contact us</li>
+        <ul className='nav-icons'>
+          <li><BsFacebook /></li>
+          <li><BsInstagram /></li>
+          <li><BsGithub /></li>
+          <li><BsTwitter /></li>
+          <li><BsTelegram /></li>
+        </ul>
       </ul>
 
-      <div className="burges">
-        <GiHamburgerMenu className='burg'/>
+      <div className="burges" onClick={openModal}>
+        <GiHamburgerMenu className='burg' />
       </div>
     </nav>
   )
